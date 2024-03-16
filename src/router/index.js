1c25e80b92bfcafd47cf10345c25121d868c731e
Gustavo Ariel Coronel <ariel256@gmail.com>
2024-03-16 16:58:11 -0300

    Added the page 404 not found

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { 
+        path: '/:pathMatch(.*)*', 
+        name: '404', 
+        component: () => import('../views/NotFound.vue')
+    },
     {
       path: '/',
       name: 'home',
@@ -20,7 +25,7 @@ const router = createRouter({
         path: "/gifs",
         name: "gifs",
         component: () => import('../views/GifsView.vue')
-    }
+    },
   ]
 })
 
