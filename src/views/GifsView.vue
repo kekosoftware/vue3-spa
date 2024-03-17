0a75331a0e540bdb4fb96db4b2af790815c49462
Gustavo Ariel Coronel <ariel256@gmail.com>
2024-03-17 16:03:48 -0300

    Apply lazing loading

--- src/views/GifsView.vue
+++ src/views/GifsView.vue
@@ -1,9 +1,39 @@
 <template>
-    <h1>Gifs Page</h1>
+    <h1>Search Gifs</h1>
+    <div class="row">
+        <div class="col-12 col-md-4 g-3" v-for="gif in gifs" :key="gif.id">
+            <div class="card">
+                <img height="250" 
+                    class="card-img-top" 
+                    :src="gif.images.original.url" 
+                    :alt="gif.id"
+                    loading="lazy"
+                />
+                <div class="card-body">
+                    <h5 class="card-title">{{ gif.title }}</h5>
+                    
+                    <a href="#" class="btn btn-primary">Download</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
+    data: () => ({
+        gifs: []
+    }),
+    created() {
+        this.getGifs()
+    },
+    methods: {
+        async getGifs() {
+            const res = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=j7BlDSJiFihdSx0rRijxCjbMVHxrYh40&q=picachu`)
+            const { data } = await res.json()
 
+            this.gifs = data
+        }
+    }
 }
 </script>
