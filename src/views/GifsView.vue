bdfaea9db1b4df795b9c46f73f88baa596fcba82
Gustavo Ariel Coronel <ariel256@gmail.com>
2024-03-17 17:13:05 -0300

    Added the sweetAlert

--- src/views/GifsView.vue
+++ src/views/GifsView.vue
@@ -1,38 +1,51 @@
 <template>
     <h1>Search Gifs</h1>
+    <search @buscar="getGifs" />
+    <loading v-show="loading" />
     <div class="row">
         <div class="col-12 col-md-4 g-3" v-for="gif in gifs" :key="gif.id">
-            <div class="card">
-                <img height="250" 
-                    class="card-img-top" 
-                    :src="gif.images.original.url" 
-                    :alt="gif.id"
-                    loading="lazy"
-                />
-                <div class="card-body">
-                    <h5 class="card-title">{{ gif.title }}</h5>
-                    
-                    <a href="#" class="btn btn-primary">Download</a>
-                </div>
-            </div>
+            <card :gif="gif" />
         </div>
     </div>
 </template>
 
 <script>
+import Card from '@/components/Card.vue'
+import Search from '@/components/Search.vue'
+import Loading from '@/components/Loading.vue'
+import Swal from 'sweetalert2'
+
 export default {
+    components: { Card, Search, Loading},
     data: () => ({
-        gifs: []
+        gifs: [],
+        loading: true,
     }),
     created() {
         this.getGifs()
     },
     methods: {
-        async getGifs() {
-            const res = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=j7BlDSJiFihdSx0rRijxCjbMVHxrYh40&q=picachu`)
+        async getGifs(busqueda = 'pokemon') {
+            if (busqueda.trim() === '') {
+                Swal.fire({
+                    title: "Búsqueda vacía",
+                    icon: "error",
+                    showConfirmButton: false,
+                    showCloseButton: true,
+                    timer: 3000,
+                    timerProgressBar: true
+                })
+                return
+            }
+            this.loading = true
+            
+            const res = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=j7BlDSJiFihdSx0rRijxCjbMVHxrYh40&q=${busqueda}`)
+            
             const { data } = await res.json()
-
+            
             this.gifs = data
+            
+            this.loading = false
         }
     }
 }
